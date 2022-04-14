81cb22eac702ac271382e47cd20c5f0ae34df09f
Oluwamuyiwa Samuel Adepoju <99486671+codermuyi@users.noreply.github.com>
2022-04-14 01:55:49 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import "./App.css"
+import ThemeToggle from "./components/ThemeToggle"
+import Info from "./components/Info"
+import About from "./components/About"
+import Interests from "./components/Interests"
+// import Projects from "./components/Projects"
+import Footer from "./components/Footer"
 
-function App() {
+const App = () => {
+  const [isDarkMode, setDarkMode] = useState(false)
+  const [appClassName, setAppClassName] = useState("App")
+
+  const changeTheme = (event) => setDarkMode(event.target.checked)
+
+  useEffect(() => {
+    setAppClassName(isDarkMode ? "App dark-app" : "App")
+  }, [isDarkMode])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>@codermuyi</h1>
-      </header>
+    <div className={appClassName}>
+      <ThemeToggle changeTheme={changeTheme} />
+      <Info />
+      <About />
+      <Interests />
+      {/* <Projects /> */}
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

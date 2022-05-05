9101f9d4da9af55b3057f3b6cfbfe77e9ebf11d7
coder-muyi <kasopeadeola@gmail.com>
2022-05-06 00:53:22 +0100

    Added tab feature to project list

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,64 @@
 import { ReactComponent as Arrow } from "../assets/icons/arrow-right.svg"
 import { ReactComponent as Bg } from "../assets/project-bg.svg"
 import { projectsList } from "../assets/data"
+import { useState, useRef, useEffect } from "react"
 
 const Projects = () => {
+  const [isLiFocused, setIsLiFocus] = useState({})
+  // const [focusClassName, setFocusClassName] = useState("")
+  const element = useRef({})
+
+  useEffect(() => {
+    const lis = element.current.children
+   
+    Array.from(lis, li => {
+      const isFocused = li === document.activeElement
+      setIsLiFocus(prev => ({...prev, [li.id]: isFocused}))
+      return ""
+    })
+  }, [])
+
+  function checkFocus(event) {
+    event.stopPropagation()
+    const {type, target} = event
+    
+    setIsLiFocus(prev => ({
+      ...prev,
+      [target.id]: !prev[target.id]
+    }))
+
+    if (type === "focus") {
+      // setFocusClassName("project-focus")
+      target.classList.add("project-focus")
+    } else {
+      target.classList.remove("project-focus")
+    }
+  }
+
+  console.log(isLiFocused)
+
+  function removeLiFocusClass(event) {
+    // setFocusClassName("")
+    const ancestor = event.nativeEvent.path[4]
+    ancestor.classList.remove("project-focus")
+  }
 
   return (
     <div className="Projects">
       <h2>My Projects</h2>
       <Bg />
-      <ul className="projects-list">
+      <ul className="projects-list" ref={element}>
         {
           projectsList.map((project, key) => 
-            <li className="project" key={key} tabIndex="0">
+            <li 
+              className="project"
+              id={key}
+              key={key} 
+              tabIndex="0"
+              onFocus={checkFocus}
+            >
               <p className="project-title">{project.name}</p>
-              <div className="project-preview" tabIndex="0">
+              <div className="project-preview">
                 <img 
                   src={project.imgSrc}
                   alt={project.name}
@@ -21,7 +66,9 @@ const Projects = () => {
                   loading="lazy"
                 />
                 <div className="project-overlay">
-                  <button className="go-to"><a href={project.url}>Visit site <Arrow /></a></button>
+                  <button className="go-to" tabIndex={-1}>
+                    <a href={project.url} onBlur={removeLiFocusClass}>Visit site <Arrow /></a>
+                  </button>
                 </div>
               </div>
             </li>

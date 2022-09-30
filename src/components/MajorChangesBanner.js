31248f98d002c2df29f7ce3b0a0179c5d71d08b8
coder-muyi <kasopeadeola@gmail.com>
2022-09-30 19:12:02 +0100

    Changed Color

--- src/components/MajorChangesBanner.js
+++ src/components/MajorChangesBanner.js
@@ -9,18 +9,19 @@ const MajorChangesBanner = () => {
 }
 
 const Container = styled.div`
-  background-color: rgb(248,180,1);
+  /* background-color: rgb(248,180,1); */
+  background-color: var(--sec-color);
   padding: 3rem 2rem 2rem;
   margin: 0;
   overflow: hidden;
   animation: hide-banner 3s ease-out 4s 1;
   animation-fill-mode: forwards;
-
   position: fixed;
   z-index: 1000;
   left: 0;
   right: 0;
   top: 0;
+  box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.2);
 
   p {
     font-weight: bold;

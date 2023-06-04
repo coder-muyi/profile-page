05c10b1c7c8e9b1533b61c78970d5c2b6489718d
codermuyi <kasopeadeola@gmail.com>
2023-06-04 18:21:40 +0100

    Renamed home.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Home from 'pages/home';
+import Home from 'pages/homepage';
 import Project from 'pages/project';
 import SharedLayout from 'components/SharedLayout';
 import GlobalStyles from 'components/GlobalStyles';
